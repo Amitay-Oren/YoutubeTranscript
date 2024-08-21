519b1903e8a2af33127574e7b23ff1d43c85127e
Amitay Oren <amitay.oren@gmail.com>
2024-08-21 17:09:43 +0300

    Add files via upload

---
+++ Server/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from youtube_transcript_api import YouTubeTranscriptApi
+
+app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
+
+
+@app.route("/get_transcript", methods=["GET"])
+def get_transcript():
+    video_id = request.args.get("video_id")
+    if not video_id:
+        return jsonify({"error": "Video ID is required"}), 400
+
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        return jsonify({"transcript": transcript})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
